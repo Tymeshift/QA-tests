7411bb28492829e0615c7004e99f4af870675188
Stefan Vilimonovic <55278904+StefanVilimonovic@users.noreply.github.com>
2021-08-09 12:42:06 +0200

    Demo site
    Demo site for practical test

---
+++ Register.js
@@ -0,0 +1,114 @@
+/**
+ * Created by Sujith Alla on 4/22/2016.
+ */
+
+$(document).ready(function()
+
+{
+
+    $("#secondpassword").keypress(function(evt)
+    {
+        if (evt.keyCode==13)
+        {
+
+            var password1= document.getElementById("firstpassword").value;
+            var password2=document.getElementById("secondpassword").value;
+
+            if (!(password1==password2))
+            {
+                input.setCustomValidity('Passwords dont match');
+
+            }
+            else
+            {
+                $("#submitbtn").click();
+            }
+
+
+        }
+
+    })
+
+
+    $(".avddbl").on('dblclick',function()
+    {
+        evt.stopPropagation();
+        evt.preventDefault();
+    })
+    $(document).on('click',function()
+    {
+     if ($("ul.ui-corner-all")[0])
+     {
+         $("ul.ui-corner-all")[0].parentElement.style.display="none";
+     }
+
+    })
+
+
+
+
+    $.getJSON('https://restcountries.eu/rest/v1/all').success(function(data)
+    {
+
+      var cdd= $('#countries');
+        for  ( var i=0; i< data.length;i++)
+        {
+            var countryname= data[i].name;
+        cdd.append($("<option></option>").attr("value",countryname).text(countryname));
+        }
+
+    });
+
+
+
+    $.getJSON('Languages.json').success(function(data)
+    {
+
+
+
+        var cdd= $('#Languages');
+        for  ( var i=0; i< data.length;i++)
+        {
+            var Language= data[i].name;
+            cdd.append($("<option></option>").attr("value",Language).text(Language));
+        }
+
+    }).error(function(err)
+    {
+
+       // console.log(JSON.stringify(err));
+    });
+
+    $.getJSON('Skills.json').success(function(data)
+    {
+
+
+        var cdd= $('#Skills');
+        for  ( var i=0; i< data.length;i++)
+        {
+            var skill= data[i].name;
+            cdd.append($("<option></option>").attr("value",skill).text(skill));
+        }
+
+    }).error(function(err)
+    {
+
+        console.log(JSON.stringify(err));
+    });
+
+
+
+
+    var yb= $('#yearbox');
+    var min=1916;
+    var max= 2016;
+
+    for (min; min<max;min++)
+    {
+        yb.append($("<option></option>").attr("value",min).text(min));
+
+    }
+
+
+
+});
